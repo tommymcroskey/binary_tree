aebb8aee5cb49ac8777dd1512527e41345a30ddb
tommymcroskey <mcroskey.thomas@gmail.com>
2024-01-29 21:59:34 -0700

    added counter for repeating values and continued insert function

--- binary_tree.py
+++ binary_tree.py
@@ -1,6 +1,7 @@
 class binary_tree_node:
 
     def __init__(self, value, left_child, right_child):
+        self.count = 1
         self.value = value
         self.left_child = left_child
         self.right_child = right_child
@@ -13,7 +14,26 @@ class binary_tree:
         
     
     def insert(self, element_to_insert):
+        #this implementation will keep a count of repeat values
+
         new_node = binary_tree_node(element_to_insert)
         current = self.root
-        while current is not None:
+
+        while current:
+
+            if new_node.value < current.value:
+                if current.left_child:
+                    current = current.left_child
+                else:
+                    current.left_child = new_node
+
+            elif new_node.value > current.value:
+                if current.right_child:
+                    current = current.right_child
+                else:
+                    current.right_child = new_node
+
+            elif new_node.value == current.value:
+                current.count = current.count + 1
+                break
             
